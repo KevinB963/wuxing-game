7b59d9254a9679c834df738100273fed58c06bf6
Kevin B <143439305+KevinB963@users.noreply.github.com>
2026-04-26 13:14:41 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Wuxing elements</h1>
+      <div>
+        <div>
+          <div>
+            <div>Player 1: 0 </div>
+            <div></div>
+          </div>
+          <div> Computer: 0 </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
