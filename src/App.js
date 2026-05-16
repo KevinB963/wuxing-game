8a33d0a2d12841faaf730f1fc19bd8375407136f
KevinB <kevinbui95@hotmail.com>
2026-05-16 13:09:45 -0400

    game play logic halfway completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+//an array for choosing the choice (both for player and
+//computer)
+const choice = ["Metal", "Water", "Wood", "Fire", "Earth"]
 
 function App() {
+  //variables containing player, computer, and their scores.
+  const [playerchoice, setplayerchoice] = useState("")
+  const [computerchoice, setcomputerchoice] =useState("")
+  const [playerscore, setplayerscore] = useState(0)
+  const [computerscore, setcomputerscore] = useState(0)
+  
+  const GamePlay = (choice) => {
+    //randomizer function for computer choice
+    computerchoice = choice[Math.floor(Math.random() * choice.length)]
+    // set player's choice based on the buttons clicked
+    setplayerchoice(choice) 
+    // set computer choice randomly based on previous function.
+    setcomputerchoice(computerchoice) 
+
+    //loop until 3 wins for either player or computer 
+    while( playerscore || computerscore < 3){
+      //if both choices are the same then no points
+      if (playerchoice == computerchoice){
+
+      }
+    }
+  }
   return (
     <div>
-      <h1>Wuxing elements</h1>
+      <div> Player's score: {playerscore} </div>
+      <div> Computer's score {computerscore} </div>
       <div>
-        <div>
-          <div>
-            <div>Player 1: 0 </div>
-            <div></div>
-          </div>
-          <div> Computer: 0 </div>
-        </div>
-      </div>
-      <div>
-        <button> Metal </button>
-        <button> Water </button>
-        <button> Wood </button>
-        <button> Fire </button>
-        <button> Earth </button>
+        <button onClick={setplayerchoice} > Metal  </button>
+        <button onClick={setplayerchoice} > Water </button>
+        <button onClick={setplayerchoice} > Wood </button>
+        <button onClick={setplayerchoice} > Fire </button>
+        <button onClick={setplayerchoice} > Earth </button>
       </div>
     </div>
   );
